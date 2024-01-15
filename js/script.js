5cf8e6ac8a3affb86a1f42767ffdc482c43f9ef8
lilycm <lilychakma733@gmail.com>
2024-01-15 11:52:08 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination1",
+      clickable: true,
+    },
+  });
